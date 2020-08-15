2e81652af8d4211027b8a31c8c9b067724e0d5ce
shubham giri <41017859+ishubs@users.noreply.github.com>
2020-08-15 17:58:50 +0530

    Updates

---
+++ precache-manifest.2d058154ec612897045cecb4d23ede6a.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c7edafd30a77eb0b3e9b4c67a8ba3a3",
+    "url": "/passportdemo/index.html"
+  },
+  {
+    "revision": "60524617f317d239a70e",
+    "url": "/passportdemo/static/css/2.92bcdaea.chunk.css"
+  },
+  {
+    "revision": "c709467910a7e6c932ff",
+    "url": "/passportdemo/static/css/main.a612d558.chunk.css"
+  },
+  {
+    "revision": "60524617f317d239a70e",
+    "url": "/passportdemo/static/js/2.192fafc7.chunk.js"
+  },
+  {
+    "revision": "c709467910a7e6c932ff",
+    "url": "/passportdemo/static/js/main.a77022f3.chunk.js"
+  },
+  {
+    "revision": "dd63d7869e1f208c4124",
+    "url": "/passportdemo/static/js/runtime-main.05c9b630.js"
+  },
+  {
+    "revision": "45e6b951b6d7da2d118e5008b5e04325",
+    "url": "/passportdemo/static/media/Maya-avatar.45e6b951.jpg"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/passportdemo/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/passportdemo/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/passportdemo/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/passportdemo/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/passportdemo/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/passportdemo/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/passportdemo/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/passportdemo/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/passportdemo/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/passportdemo/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/passportdemo/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/passportdemo/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/passportdemo/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/passportdemo/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/passportdemo/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/passportdemo/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/passportdemo/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/passportdemo/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/passportdemo/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/passportdemo/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/passportdemo/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/passportdemo/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/passportdemo/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/passportdemo/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/passportdemo/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/passportdemo/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/passportdemo/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/passportdemo/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/passportdemo/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/passportdemo/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/passportdemo/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/passportdemo/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/passportdemo/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/passportdemo/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/passportdemo/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/passportdemo/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/passportdemo/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "7f6de6cef4b59e7f16428c448fa1a7a1",
+    "url": "/passportdemo/static/media/logo.7f6de6ce.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/passportdemo/precache-manifest.2d058154ec612897045cecb4d23ede6a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/passportdemo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
